db45790bf7aad81139d9f7092237cb11edef62f8
i-like-dev <136171623+i-like-dev@users.noreply.github.com>
2023-10-31 11:59:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ function calculateResult() {
         document.getElementById('display').value = 'Error';
     }
 }
+
+function copyAnswer() {
+    const answer = document.getElementById('display').value;
+    navigator.clipboard.writeText(answer)
+        .then(() => alert('Answer copied to clipboard'))
+        .catch(err => console.error('Unable to copy text', err));
+}
+
+function calculateFromInput() {
+    const userInput = document.getElementById('userInput').value;
+    try {
+        const result = eval(userInput);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
